3a372ae5f973087488c28243eb4bdb5705c7aa2f
dhruvraj sarvaiya <dhruvraj.sarvaiya@gmail.com>
2013-08-11 02:27:14 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobsRubynow::Application.config.secret_token = '6be4b59af9308b04f721e57b951406870d4062301cd0ac52e195909a8e7ce578ee2af9e35dc722770b75a5dbba0cd56cbcbcc9c2c468aaa95bcf71df9e0a1f73'
